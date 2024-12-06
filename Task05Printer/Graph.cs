32c9a2dd6ee0d33ee601536d8cf901cd332fb5ee
Michael Koudela <xkoudel2@fi.muni.cz>
2024-12-06 02:09:31 +0100

    Add task 05 solution

---
+++ Task05Printer/Graph.cs
@@ -0,0 +1,64 @@
+namespace AdventCalendar2024.Task05Printer;
+
+public class Graph
+{
+    public List<(int From, int To)> Edges { get; } = [];
+
+    // Vertex and list of vertices it is accessible from
+    private Dictionary<int, HashSet<int>> _vertices = new();
+
+    public void AddEdge(int from, int to)
+    {
+        Edges.Add((from, to));
+    }
+
+    private int FindVertexWithoutParent()
+    {
+        return _vertices.First(v => v.Value.Count == 0).Key;
+    }
+
+    private void RemoveVertex(int vertexToRemove)
+    {
+        _vertices.Remove(vertexToRemove);
+        foreach (var vertexDetail in _vertices)
+        {
+            vertexDetail.Value.Remove(vertexToRemove);
+        }
+    }
+
+    public List<int> GetTopologicalSort(HashSet<int> usedVertices)
+    {
+        _vertices = new();
+        
+        foreach (var vertex in usedVertices)
+        {
+            _vertices.Add(vertex, []);
+        }
+        
+        foreach (var (from, to) in Edges)
+        {
+            if (!usedVertices.Contains(from) || !usedVertices.Contains(to))
+            {
+                continue;
+            }
+            if (_vertices.ContainsKey(to))
+            {
+                _vertices[to].Add(from);
+            }
+            else
+            {
+                _vertices.Add(to, [from]);
+            }
+        }
+
+        List<int> topologicalSort = new();
+        while (_vertices.Count > 0)
+        {
+            int vertex = FindVertexWithoutParent();
+            topologicalSort.Add(vertex);
+            RemoveVertex(vertex);
+        }
+        
+        return topologicalSort;
+    }
+}
---
+++ Task05Printer/Task05Solver.cs
@@ -0,0 +1,96 @@
+﻿using AdventCalendar2024.Shared;
+
+namespace AdventCalendar2024.Task05Printer;
+
+public class Task05Solver : ITaskSolver
+{
+    private readonly Graph _rules = new();
+    private readonly List<List<int>> _printPageList = new();
+
+    public void LoadTaskDataFromFile(string filePath)
+    {
+        using var reader = new StreamReader(filePath);
+        string? line;
+
+        while ((line = reader.ReadLine()) != null && !string.IsNullOrWhiteSpace(line))
+        {
+            var numbers = line.Split("|");
+            if (int.TryParse(numbers[0], out int a) && int.TryParse(numbers[1], out int b))
+            {
+                _rules.AddEdge(a, b);
+            }
+        }
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            var numbers = line.Split(",");
+            _printPageList.Add(numbers.ToList().Select(int.Parse).ToList());
+        }
+    }
+
+    private bool SequenceFulfillsRules(List<int> sequence)
+    {
+        foreach (var rule in _rules.Edges)
+        {
+            int fromIndex = sequence.IndexOf(rule.From);
+            int toIndex = sequence.IndexOf(rule.To);
+            if (fromIndex > -1 && toIndex > -1 && fromIndex > toIndex)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int CountMiddleNumbersOfRightSequences()
+    {
+        return _printPageList.Aggregate(0, (prev, sequence) =>
+        {
+            if (SequenceFulfillsRules(sequence))
+            {
+                Console.WriteLine($"Sequence passed: {string.Join(", ", sequence)}");
+                return prev + sequence[sequence.Count / 2];
+            }
+
+            return prev;
+        });
+    }
+
+    private List<int> CreateFixedSequence(List<int> originalSequence)
+    {
+        return _rules.GetTopologicalSort(originalSequence.ToHashSet());
+    }
+
+    private int CountMiddleNumbersOfFixedSequences()
+    {
+        return _printPageList.Aggregate(0, (prev, sequence) =>
+        {
+            if (!SequenceFulfillsRules(sequence))
+            {
+                // Console.WriteLine($"Sequence failed: {string.Join(", ", sequence)}");
+                var correctedSequence = CreateFixedSequence(sequence);
+                // Console.WriteLine($"Sequence corrected: {string.Join(", ", correctedSequence)}");
+                if (!SequenceFulfillsRules(correctedSequence))
+                {
+                    Console.Error.WriteLine($"Correcting failed for sequence {string.Join(", ", sequence)}");
+                }
+
+                return prev + correctedSequence[correctedSequence.Count / 2];
+            }
+
+            return prev;
+        });
+    }
+
+    private void PrepareBeforeTask()
+    {
+    }
+
+    public void SolveTask()
+    {
+        PrepareBeforeTask();
+        Console.WriteLine($"Middle sequence score: {CountMiddleNumbersOfRightSequences()}");
+        Console.WriteLine($"Middle corrected sequence score: {CountMiddleNumbersOfFixedSequences()}");
+    }
+}
