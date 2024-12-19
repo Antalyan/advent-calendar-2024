2605112f49537a3b81baaf4debbc233063a5303b
Michael Koudela <xkoudel2@fi.muni.cz>
2024-12-19 10:17:09 +0100

    Add task 12a solution

---
+++ Task12GardenArea/Task12Solver.cs
@@ -0,0 +1,145 @@
+﻿using AdventCalendar2024.Shared;
+
+namespace AdventCalendar2024.Task12GardenArea;
+
+public class Task12Solver : ITaskSolver
+{
+    private readonly Dictionary<Coordinate, char> _garden = new();
+    private Coordinate _maxPosition;
+
+    public void LoadTaskDataFromFile(string filePath)
+    {
+        using var reader = new StreamReader(filePath);
+        string? line;
+
+        int lineNumber = 0;
+        while ((line = reader.ReadLine()) != null)
+        {
+            // Positions are numbered from top left corner
+            for (int charPosition = 0; charPosition < line.Length; charPosition++)
+            {
+                _garden[(charPosition, lineNumber)] = line[charPosition];
+            }
+
+            _maxPosition = (line.Length - 1, lineNumber);
+            lineNumber++;
+        }
+    }
+
+    //TODO: move to common
+    private bool CoordinateIsInside(Coordinate coordinate)
+    {
+        return coordinate.X >= 0 && coordinate.Y >= 0 && coordinate.X <= _maxPosition.X &&
+               coordinate.Y <= _maxPosition.Y;
+    }
+
+    private int CountClusterPrice(Coordinate clusterStartPos, HashSet<Coordinate> visited)
+    {
+        int perimeter = 0;
+        HashSet<Coordinate> seen = [clusterStartPos];
+        Queue<Coordinate> coordQueue = new();
+        coordQueue.Enqueue(clusterStartPos);
+
+        while (coordQueue.Count > 0)
+        {
+            var currentPosition = coordQueue.Dequeue();
+            foreach (var positionMod in CoordinationHelper.GetLineCoordModifiers())
+            {
+                Coordinate neighborPosition =
+                    (currentPosition.X + positionMod.X, currentPosition.Y + positionMod.Y);
+                if (CoordinateIsInside(neighborPosition) && _garden[neighborPosition] == _garden[currentPosition])
+                {
+                    if (!seen.Contains(neighborPosition))
+                    {
+                        coordQueue.Enqueue(neighborPosition);
+                        seen.Add(neighborPosition);
+                    }
+                }
+                else
+                {
+                    perimeter += 1;
+                }
+            }
+        }
+
+        visited.UnionWith(seen);
+        return perimeter * seen.Count;
+    }
+
+    private int CountAdvancedClusterPrice(Coordinate clusterStartPos, HashSet<Coordinate> visited)
+    {
+        int perimeter = 0;
+
+        Dictionary<Coordinate, HashSet<Coordinate>> ignoredDirectionMods = new();
+        HashSet<Coordinate> seen = [clusterStartPos];
+        Queue<Coordinate> coordQueue = new();
+        coordQueue.Enqueue(clusterStartPos);
+
+        while (coordQueue.Count > 0)
+        {
+            var currentPosition = coordQueue.Dequeue();
+
+            HashSet<Coordinate> currentLinePositions = new();
+
+            foreach (var positionMod in CoordinationHelper.GetLineCoordModifiers())
+            {
+                Coordinate neighborPosition =
+                    (currentPosition.X + positionMod.X, currentPosition.Y + positionMod.Y);
+                if (CoordinateIsInside(neighborPosition) && _garden[neighborPosition] == _garden[currentPosition])
+                {
+                    if (!seen.Contains(neighborPosition))
+                    {
+                        coordQueue.Enqueue(neighborPosition);
+                        seen.Add(neighborPosition);
+                    }
+                }
+                else
+                {
+                    currentLinePositions.Add(positionMod);
+
+                    if (!ignoredDirectionMods.TryGetValue(currentPosition, out var ignoredDirs) ||
+                        !ignoredDirs.TryGetValue(positionMod, out var _))
+                    {
+                        perimeter += 1;
+                    }
+                }
+            }
+
+            foreach (var positionMod in CoordinationHelper.GetLineCoordModifiers())
+            {
+                Coordinate neighborPosition =
+                    (currentPosition.X + positionMod.X, currentPosition.Y + positionMod.Y);
+                if (!ignoredDirectionMods.TryAdd(neighborPosition, [..currentLinePositions]))
+                {
+                    ignoredDirectionMods[neighborPosition].UnionWith(currentLinePositions);
+                }
+            }
+        }
+
+        visited.UnionWith(seen);
+        return perimeter * seen.Count;
+    }
+
+    private int CountAreaPrice()
+    {
+        HashSet<Coordinate> visited = new();
+        // C# defers where execution (i.e. visited condition is not called until the particular key is needed)
+        return _garden.Keys.Where(pos => !visited.Contains(pos)).Sum(pos => CountClusterPrice(pos, visited));
+    }
+
+    private int CountAdvancedAreaPrice()
+    {
+        HashSet<Coordinate> visited = new();
+        return _garden.Keys.Where(pos => !visited.Contains(pos)).Sum(pos => CountAdvancedClusterPrice(pos, visited));
+    }
+
+    public long SolveTaskP1()
+    {
+        return CountAreaPrice();
+    }
+
+    public long SolveTaskP2()
+    {
+        return CountAdvancedAreaPrice();
+    }
+}
---
+++ Task12GardenArea/Task12UnitTest.cs
@@ -0,0 +1,36 @@
+using AdventCalendar2024.Shared;
+using Xunit;
+
+namespace AdventCalendar2024.Task12GardenArea;
+
+public class Task12UnitTest
+{
+    private static ITaskSolver CreateSolver(string fileName)
+    {
+        string sourceFile = Path.Combine(Constants.DataFilePath, $"{fileName}.txt");
+        ITaskSolver taskSolver = new Task12Solver();
+        taskSolver.LoadTaskDataFromFile(sourceFile);
+        return taskSolver;
+    }
+    
+    [Theory]
+    [InlineData("T12-Sample01", 140)]
+    [InlineData("T12-Sample02", 1930)]
+    public void P1Test(string fileName, long expectedResult)
+    {
+        var solver = CreateSolver(fileName);
+        Assert.Equal(solver.SolveTaskP1(), expectedResult);
+    }
+    
+    [Theory]
+    [InlineData("T12-Sample01", 80)]
+    [InlineData("T12-Sample02", 1206)]
+    [InlineData("T12-Sample03", 236)]
+    [InlineData("T12-Sample04", 368)]
+    [InlineData("T12-Sample05", 436)]
+    public void P2Test(string fileName, long expectedResult)
+    {
+        var solver = CreateSolver(fileName);
+        Assert.Equal(solver.SolveTaskP2(), expectedResult);
+    }
+}
